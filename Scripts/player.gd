0662c9a16124ea752f7c09871603571329f7418c
Alex Khon <honaleksandr4@gmail.com>
2025-07-19 00:51:51 +0300

    Update

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,17 +1,31 @@
 extends CharacterBody3D
 
 var speed = 1
-var rotation_speed = 0.02
+var sensivity = 0.003
+var rotation_x = 0
+var rotation_y = 0
 @onready var walk_animation = $animation
 @onready var step_sound = $steps_sound
 
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func _physics_process(delta: float) -> void:
-	var input_dir = Input.get_vector("move_forward", "move_back", "move_left", "move_right")
-	var dir = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var move_dir = Vector3.ZERO
+	if Input.is_action_pressed("move_forward"):
+		move_dir += -transform.basis.z
+	if Input.is_action_pressed("move_back"):
+		move_dir += transform.basis.z
+	if Input.is_action_pressed("move_left"):
+		move_dir += -transform.basis.x
+	if Input.is_action_pressed("move_right"):
+		move_dir += transform.basis.x
+		
+	move_dir = move_dir.normalized()
 	
-	if dir:
-		velocity.z = dir.x * speed
-		velocity.x = dir.z * speed
+	if move_dir:
+		velocity.z = move_dir.z * speed
+		velocity.x = move_dir.x * speed
 		walk_animation.play("walk")
 	else:
 		velocity.x = 0
@@ -21,3 +35,13 @@ func _physics_process(delta: float) -> void:
 		
 	move_and_slide()
 	
+func _input(e: InputEvent) -> void:
+	if e is InputEventMouseMotion:
+		rotation_x -= e.relative.y * sensivity
+		rotation_y -= e.relative.x * sensivity
+		
+		if rotation_x > 1: rotation_x = 1
+		if rotation_x < -1: rotation_x = -1
+		
+		transform.basis = Basis(Vector3(0,1,0), rotation_y)
+		$camera.transform.basis = Basis(Vector3(1,0,0), rotation_x)
--- Scripts/prologue.gd
+++ Scripts/prologue.gd
@@ -5,6 +5,10 @@ var taps_counter = 0
 @onready var s1 = $Sprite1
 @onready var s2 = $Sprite2
 @onready var s3 = $Sprite3
+@onready var k = $keyboard_sound
+
+func _ready() -> void:
+	k.play()
 
 func check_and_change_sprite():
 	if taps_counter == 0:
@@ -21,4 +25,6 @@ func check_and_change_sprite():
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("continue"):
 		taps_counter += 1
+		if taps_counter < 3:
+			k.play()
 	check_and_change_sprite()
